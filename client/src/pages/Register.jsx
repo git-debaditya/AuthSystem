bc8b6ba3e56930a33ea30afc33ab8de992cace8b
git-debaditya <debaditya.bhattacharjee.4521@gmail.com>
2026-03-04 22:00:33 +0530

    Register page created

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,17 @@
 //On submit: POST /auth/register
 //On success: redirect to /dashboard (since your backend auto-logs in on register)
 
-function Register() {
-    return (
-        <div>Register Yourself</div>
-    )
+import { useState } from "react";                    //track form fields & errors
+import { useNavigate, Link } from "react-router-dom";     //redirect after registration
+import api from "../api";                          //ensures cookies are included automatically
+
+export default function Register () {
+    const navigate = useNavigate("");         //for redirecting after registration
+    const [email, setEmail] = useState("");         //initialize state for email
+    const [password, setPassword] = useState("");   //initialize state for password
+    const [confirmPassword, setConfirmPassword ] = useState("");  //initialize state for confirm password
+    const [error, setError] = useState("");         //initialize state for error message
+    const [loading, setLoading] = useState(false);  //initialize state for loading status
+
+    
 }
-export default Register;
