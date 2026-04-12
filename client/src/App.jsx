d4930193fcb486b510ce7e3f5fa1e2237486fb68
git-debaditya <debaditya.bhattacharjee.4521@gmail.com>
2026-04-12 16:08:43 +0100

    Add basename to React Router for GitHub Pages subdirectory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/AuthSystem/">
       <Routes>
         <Route path= "/" element ={<Navigate to="/login" replace />} />   {/* Redirect root to login */}
         <Route path="/register" element={<Register />} />
